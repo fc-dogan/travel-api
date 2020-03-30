340a651d5e8994672ac9a0257e9df768f20dce8d
Fatma <fceyda@gmail.com>
2020-03-30 13:35:54 -0700

    modify ReviewsController.cs

--- Travel/Controllers/ReviewsController.cs
+++ Travel/Controllers/ReviewsController.cs
@@ -1,57 +1,61 @@
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using Travel.Models;
+  
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Travel.Models;
 
-// namespace Travel.Controllers
-// {
-//   [Route("api/[controller]")]
-//   [ApiController]
-//   public class ReviewsController : ControllerBase
-//   {
-//     private TravelContext _db;
+namespace Travel.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ReviewsController : ControllerBase
+  {
+    private TravelContext _db;
 
-//     public ReviewsController(TravelContext db)
-//     {
-//       _db = db;
-//     }
+    public ReviewsController(TravelContext db)
+    {
+      _db = db;
+    }
 
-//     // GET api/Reviews
-//     [HttpGet]
-//     public ActionResult<IEnumerable<Review>> Get()
-//     {
-//       return _db.Reviews.ToList();
-//     }
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<Review>> Get()
+    {
+      return _db.Reviews.ToList();
+    }
 
-//     // POST api/Reviews
-//     [HttpPost]
-//     public void Post([FromBody] Review review)
-//     {
-//       _db.Reviews.Add(review);
-//       _db.SaveChanges();
-//     }
+   
+    [HttpPost]
+    public void Post([FromBody] Review review)
+    {
+      _db.Reviews.Add(review);
+      _db.SaveChanges();
+    }
 
-//     [HttpGet("{id}")]
-//     public ActionResult<Destination> Get(int id)
-//     {
-//       return _db.Destinations.FirstOrDefault(entry => entry.DestinationId == id);
-//     }
+   
+    [HttpGet("{id}")]
+    public ActionResult<Review> Get(int id)
+    {
+      return _db.Reviews.FirstOrDefault(entry => entry.ReviewId == id);
+    }
 
-//     [HttpPut("{id}")]
-//     public void Put(int id, [FromBody] Review review)
-//     {
-//       review.ReviewId = id;
-//       _db.Entry(review).State = EntityState.Modified;
-//       _db.SaveChanges();
-//     }
+    
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Review review)
+    {
+      review.ReviewId = id;
+      _db.Entry(review).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
 
-//     [HttpDelete("{id}")]
-//     public void Delete(int id)
-//     {
-//       var reviewToDelete = _db.Reviews.FirstOrDefault(entry => entry.ReviewId == id);
-//       _db.Reviews.Remove(reviewToDelete);
-//       _db.SaveChanges();
-//     }
-//   }
-// }
+   
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var reviewToDelete = _db.Reviews.FirstOrDefault(entry => entry.ReviewId == id);
+      _db.Reviews.Remove(reviewToDelete);
+      _db.SaveChanges();
+    }
+  }
+}    
