8a374054cbe7ed698ff2829bd1dab29838ee13c2
Fatma <fceyda@gmail.com>
2020-03-31 10:06:16 -0700

    update Review.cs

--- Travel/Models/Review.cs
+++ Travel/Models/Review.cs
@@ -8,15 +8,18 @@ namespace Travel.Models
   {
     public int ReviewId {get; set;}
     [Required]
+    public string Country { get; set; }
+    [Required]
+    public string City { get; set; }
+    [Required]
     [StringLength(20)]
+    
     public string Author {get; set;}
     [Required]
-    [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
     public int Rating {get; set;}
     [Required]
     public string Description {get; set;}
-    public int DestinationId {get; set;}
-    public Destination Destination {get; set;}
+  
 
   }
 }
